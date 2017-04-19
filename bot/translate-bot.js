72ad9da32c207e4c8b4becd42ab20d410fe95489
Joe Bullard <jbullard@reactive.co.jp>
2017-04-19 22:35:00 +0900

    Changed the message structure back to just being strings, not filepaths.
    Switched to  over , which can easily be run on the command line in development without needing to deal with it in the code directly.

--- bot/translate-bot.js
+++ bot/translate-bot.js
@@ -1,48 +1,24 @@
-var env = require('node-env-file');
-env(process.env.ENV || '.env')
-var fs = require('fs');
 var Botkit = require('botkit');
 var Translate = require('@google-cloud/translate')({
     keyFilename: process.env.GOOGLE_CREDENTIALS
 });
 
-// Need a registered bot with token
-if (!process.env.BOT_TOKEN) {
-  console.log('ERROR: Must define BOT_TOKEN in environment');
-  process.exit(1);
-}
+const DEFAULT_GREETING = 'Hello! My name is <@me>!'
+const DEFAULT_APOLOGY = 'Sorry! I had some trouble translating your message.'
 
-// Need to specify a target language
-if (!process.env.TARGET_LANG) {
-  console.log('ERROR: Must define TARGET_LANG in environemnt');
-  process.exit(1);
-}
+// Check if target language is supported
+Translate.getLanguages()
+  .then((data) => {
+    var codes = data[0].map((lang) => lang.code);
 
-// Target language must be supported
-Translate.getLanguages().then(function (data) {
-  var codes = data[0].map((lang) => lang.code);
+    if (codes.indexOf(process.env.TARGET_LANG) < 0) {
+      console.log(`ERROR: Invalid target language ${process.env.TARGET_LANG}`);
+      process.exit(1);
+    }
+  });
 
-  if (codes.indexOf(config.targetLang) < 0) {
-    console.log(`ERROR: Invalid target language ${process.env.TARGET_LANG}`);
-    process.exit(1);
-  }
-});
-
-GREETING = 'Hello! My name is <@me>!';
-if (process.env.GREETING)
-  GREETING = String(fs.readFileSync(process.env.GREETING))
-
-APOLOGY = 'Sorry! I had some trouble translating your message!'
-if (process.env.APOLOGY)
-  APOLOGY = String(fs.readFileSync(process.env.APOLOGY))
-
-
-/******************************************************************************/
-
-// Create the bot
 var controller = Botkit.slackbot({debug: process.env.DEBUG || false});
 
-// Connect to RTM
 controller
   .spawn({token: process.env.BOT_TOKEN})
   .startRTM((err) => {
@@ -50,19 +26,28 @@ controller
       throw new Error(err)
   });
 
-// Introduce ourselves
-controller.on('channel_joined', (bot, msg) => {
-  bot.replyPublic(msg, GREETING.replace(/<@me>/g, '<@' + bot.name + '>'));
+controller.on('channel_join', (bot, msg) => {
+  var greeting = process.env.GREETING || DEFAULT_GREETING;
+
+  bot.replyPublic(msg, greeting.replace(/<@me>/g, '<@' + bot.name + '>'));
 });
 
-// You talkin to me?
-controller.on(['direct_mention', 'direct_message'], (bot, msg) => {
+controller.on(['mention', 'direct_mention', 'direct_message'], (bot, msg) => {
   Translate.translate(msg.text, process.env.TARGET_LANG)
     .then((results) => {
-      bot.reply(msg, results[0]);
+      var translation = results[0];
+      var mentions = translation.match(/<@[^>]+>/g);
+
+      if (mentions) {
+        mentions.forEach((m) => {
+          translation = translation.replace(m, m.replace(/ /g, ''))
+        });
+      }
+
+      bot.reply(msg, translation);
     })
     .catch((err) => {
       console.log(err);
-      bot.reply(msg, APOLOGY);
+      bot.reply(msg, process.env.APOLOGY || DEFAULT_APOLOGY);
     })
 });
